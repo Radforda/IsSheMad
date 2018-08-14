103ac1a3414e226b2f2f64e966a56e9b358725b9
ashley o <oneal.ashleyc@gmail.com>
2018-08-14 11:18:10 -0400

    added angry and okay objects and the schema file for the database

---
+++ d-angry.js
@@ -0,0 +1,105 @@
+const angry = [
+
+    {
+      input: "k.",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "I'm fine",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "K",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "whatever",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "leave me alone",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "we need to talk",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "fine",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "so who is that girl",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "call me!!!",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "you can text me when you are not too busy to talk",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "go or dont, its your choice",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "No.",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "so...",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "Good night.",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "hello???",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "Why are you mad at me???",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "is is a good morning???",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "why haven't you texted me back yet???",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "where have you been???",
+      output: {angry: 1}
+    },
+  
+    {
+      input: "I guess",
+      output: {angry: 1}
+    },
+  
+  ]
+  
+  module.exports = angry
