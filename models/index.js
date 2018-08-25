09106ea61de99141190fc1a9a93c170fc068b143
Andrea Radford <radfordandrealynn@gmail.com>
2018-08-25 15:39:59 -0400

    more connection stuff

--- models/index.js
+++ models/index.js
@@ -4,12 +4,12 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.JAWSDB_URL || "development";
+var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-if (config.env.JAWSDB_URL) {
-  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(
     config.database,
